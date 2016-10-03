137908e738d161e22c99725bcc0a1286a24ef357
jscottcronin <j.scott.cronin@gmail.com>
2016-10-03 03:05:47 +0000

    adding sniffer for amazon button

---
+++ sniffer.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+
+def arp_display(pkt):
+    if pkt[ARP].op == 1: #who-has (request)
+        # print pkt[ARP].psrc
+        # if pkt[ARP].psrc == '192.168.1.149': # ARP Probe
+        if pkt[ARP].hwsrc == '50:f5:da:cc:4d:3e':
+            print "ARP Probe from: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, iface='wlan0', filter="arp", store=0, count=0)
